965c2ce7ec39906caac5d52e927b43d1bb717979
niuyi1017 <1096196462@qq.com>
2018-12-01 22:28:46 +0800

    add 1017

---
+++ PAT-Test/advanced/1017.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+
+struct node
+{
+  int come,time;
+
+}tempCustomer;
+
+bool compl(node a, node b){
+  return a.come < b.come;
+}
+
+int main(int argc, char const *argv[])
+{
+  return 0;
+}
+
