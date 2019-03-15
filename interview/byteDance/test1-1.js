bc95660e39b1d7066c56f2c99fa844d8891a26d2
niuyi1017 <1096196462@qq.com>
2019-03-15 21:17:32 +0800

    for remember the first time to ac by oj with js

--- interview/byteDance/test1-1.js
+++ interview/byteDance/test1-1.js
@@ -8,21 +8,36 @@ let lineCount = 0
 let person = 0
 let keys = []
 let testNum = 0
-let testArr = []
+let sameKeys = {}
 
 rl.on('line', data=>{
   lineCount++
   if(lineCount == 1){
     person = Number(data.trim())
   }else if(lineCount == 2){
-    keys = data.trim().split(' ').map(x => Number(x))
+    keys = data.trim().split(' ').forEach((value,index) => {
+      if(sameKeys[value]){
+        sameKeys[value].push(++index)
+      }else{
+        sameKeys[value] = [++index]
+      }
+    });
   } else if (lineCount == 3){
     testNum = Number(data.trim())
   }else{
-    testArr.push(data.trim().split(' ').map(x => Number(x)))
     testNum--
+    let [start, end, k] = data.trim().split(' ')
+    start = parseInt(start)
+    end = parseInt(end)
+    let count = 0
+    if (sameKeys[k]){
+      sameKeys[k].forEach(item => {
+        if(item<=end &&item >=start)
+        count++
+      })
+    }
+    console.log(count)
     if (testNum == 0){
-      console.log(testArr)
       process.exit(0)
     }
   }
