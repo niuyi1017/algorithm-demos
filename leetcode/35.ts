7a89ce888e16da35a7083289dc551ed1a2f4b804
niuyi1017 <1096196462@qq.com>
2024-01-03 16:54:44 +0800

    leetcode

---
+++ leetcode/35.ts
@@ -0,0 +1,15 @@
+function searchInsert(nums: number[], target: number): number {
+    let left = 0
+    let right = nums.length - 1
+    while (left <= right) {
+        let mid = left + ((right - left) >> 1)
+        if (nums[mid] < target) {
+            left = mid + 1
+        } else if (nums[mid] > target) {
+            right = mid - 1
+        } else {
+            return mid
+        }
+    }
+    return right + 1
+};
